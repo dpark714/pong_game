98af09838b3dd9a4a9db8ebe0f6309d4c8bbbc3e
Dahyeon <110885780+dpark714@users.noreply.github.com>
2023-03-12 20:46:18 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+from turtle import Screen, Turtle
+from paddle import *
+from ball import Ball
+from scoreboard import *
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("pong")
+screen.tracer(0)
+
+# paddle
+l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
+
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "e")
+screen.onkey(l_paddle.go_down, "s")
+
+game_on = True
+while game_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.y_bounce()
+
+    # Detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.x_bounce()
+
+    # Detect when paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+screen.exitonclick()
